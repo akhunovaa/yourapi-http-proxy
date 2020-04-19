8bf9b8b3b99814605d909aa0e494af8b36b0716c
Azat Akhunov <azat.akhunov@protonmail.com>
2020-04-19 18:19:28 +0300

    [NF] proxying with param data added

--- src/main/java/ru/yourapi/filter/TokenAuthenticationFilter.java
+++ src/main/java/ru/yourapi/filter/TokenAuthenticationFilter.java
@@ -1,18 +1,18 @@
 package ru.yourapi.filter;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import ru.yourapi.exception.InvalidUserApplicationSecretException;
-import ru.yourapi.exception.UserApplicationNotFoundException;
-import ru.yourapi.provider.UserApplicationSecretProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import ru.yourapi.exception.InvalidUserApplicationSecretException;
+import ru.yourapi.exception.UserApplicationNotFoundException;
+import ru.yourapi.provider.UserApplicationSecretProvider;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -47,13 +47,13 @@ public class TokenAuthenticationFilter extends GenericFilterBean {
                 Authentication auth = userApplicationSecretProvider.getAuthentication(secret);
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
-            if (allowOrigin) {
-                HttpServletResponse res = (HttpServletResponse) response;
-                res.setHeader("Access-Control-Allow-Origin", "*");
-                res.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-                res.setHeader("Access-Control-Max-Age", "3600");
-                res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-            }
+
+            HttpServletResponse res = (HttpServletResponse) response;
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+            res.setHeader("Access-Control-Max-Age", "3600");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+
             chain.doFilter(request, response);
         } catch (UserApplicationNotFoundException | InvalidUserApplicationSecretException exception) {
             LOGGER.error("Could not set user authentication in security context", exception);
