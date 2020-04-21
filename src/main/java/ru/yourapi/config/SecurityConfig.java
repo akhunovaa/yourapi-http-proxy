157f0427285381a479b5c64de4473c178ebd9ad4
Azat Akhunov <azat.akhunov@protonmail.com>
2020-04-22 02:05:08 +0300

    [NF] CORS proxying with param data added

--- src/main/java/ru/yourapi/config/SecurityConfig.java
+++ src/main/java/ru/yourapi/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -100,6 +101,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().accessDeniedHandler(accessDeniedHandler).authenticationEntryPoint(authenticationEntryPoint)
                 .and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .permitAll()
                 .antMatchers("/**")
                 .permitAll()
                 .and()
